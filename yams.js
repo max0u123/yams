c4a547d335132226bc0f71ba59a672379ce2c841
Fulldia <camille.lafourcade64@orange.fr>
2023-03-01 09:36:56 +0100

    calcul score ok

--- yams.js
+++ yams.js
@@ -19,49 +19,42 @@ function lancerDes() {
 module.exports = lancerDes;
 console.log(lancerDes());
 
-//Erreur en cas de 3 lancés
-roll(){
-    if (lances > 3) {
-      throw new Error('Cannot roll more than three times');
-    }
-}
-
 
 // Fonction pour compter le nombre de dés ayant une certaine valeur
-function compterDes(des, valeur) {
-    return des.filter(d => d === valeur).length;
+function compterDes(valeursDes, valeur) {
+    return valeursDes.filter(d => d === valeur).length;
   }
   
   // Fonction pour vérifier si une combinaison est valide
-  function estValide(des, combinaison) {
+  function estValide(valeursDes, combinaison) {
     switch (combinaison) {
       case 'brelan':
-        return des.some(d => compterDes(des, d) >= 3);
+        return valeursDes.some(d => compterDes(valeursDes, d) >= 3);
       case 'carre':
-        return des.some(d => compterDes(des, d) >= 4);
+        return valeursDes.some(d => compterDes(valeursDes, d) >= 4);
       case 'full':
-        return des.some(d => compterDes(des, d) === 3) &&
-               des.some(d => compterDes(des, d) === 2);
+        return valeursDes.some(d => compterDes(valeursDes, d) === 3) &&
+               valeursDes.some(d => compterDes(valeursDes, d) === 2);
       case 'petiteSuite':
-        return trierDes(des).join('') === '12345' ||
-               trierDes(des).join('') === '23456';
+        return trierDes(valeursDes).join('') === '12345' ||
+               trierDes(valeursDes).join('') === '23456';
       case 'grandeSuite':
-        return trierDes(des).join('') === '12345' ||
-               trierDes(des).join('') === '23456';
+        return trierDes(valeursDes).join('') === '12345' ||
+               trierDes(valeursDes).join('') === '23456';
       case 'yahtzee':
-        return des.every(d => d === des[0]);
+        return valeursDes.every(d => d === valeursDes[0]);
       default:
         return true;
     }
   }
   
   // Fonction pour calculer le score pour une combinaison
-  function calculerScore(des, combinaison) {
+  function calculerScore(valeursDes, combinaison) {
     switch (combinaison) {
       case 'brelan':
-        return des.reduce((acc, d) => acc + d, 0);
+        return valeursDes.reduce((acc, d) => acc + d, 0);
       case 'carre':
-        return des.reduce((acc, d) => acc + d, 0);
+        return valeursDes.reduce((acc, d) => acc + d, 0);
       case 'full':
         return 25;
       case 'petiteSuite':
@@ -71,7 +64,7 @@ function compterDes(des, valeur) {
       case 'yahtzee':
         return 50;
       default:
-        return des.reduce((acc, d) => acc + d, 0);
+        return valeursDes.reduce((acc, d) => acc + d, 0);
     }
   }
   
